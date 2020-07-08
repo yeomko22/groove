f0f246c0027628640fdfecf8f66d45cde37f1aaf
Hyeong Jun Kim <hjkim2246@gmail.com>
2020-07-08 15:54:25 +0900

    add travis.yaml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,11 @@
 # get git commit hashcode
 export SHA=$(git rev-parse HEAD)
 
+# api build and push
+docker build -t yeomko/groove-api:latest -t yeomko/groove-api:$SHA ./api
+docker push yeomko/groove-api:latest
+docker push yeomko/groove-api:$SHA
+
 # fileupload build and push
 docker build -t yeomko/groove-fileupload:latest -t yeomko/groove-fileupload:$SHA ./fileupload
 docker push yeomko/groove-fileupload:latest
@@ -11,7 +16,7 @@ docker build -t yeomko/groove-stream:latest -t yeomko/groove-stream:$SHA ./strea
 docker push yeomko/groove-stream:latest
 docker push yeomko/groove-stream:$SHA
 
-# api build and push
-docker build -t yeomko/groove-api:latest -t yeomko/groove-api:$SHA ./api
-docker push yeomko/groove-api:latest
-docker push yeomko/groove-api:$SHA
+kubectl apply -f k8s
+kubectl set image deployments/api-deployment api=yeomko/groove-api:$SHA
+kubectl set image deployments/fileupload-deployment fileupload=yeomko/groove-fileupload:$SHA
+kubectl set image deployments/stream-deployment stream=yeomko/groove-stream:$SHA
