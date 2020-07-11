b5657cadc47b007b4a20896c04356a908cc2166e
Hyeong Jun Kim <hjkim2246@gmail.com>
2020-07-11 15:17:51 +0900

    - 유저 아이디로 트랙 가져오는 GET API 추가 - 트랙 아이디로 트랙 정보 가져오는 GET API 추가 - 트랙 재생 횟수, 좋아요 수 증가 시키는 PUT API 추가

--- api/Controllers/TrackController.go
+++ api/Controllers/TrackController.go
@@ -7,6 +7,17 @@ import (
 	"strings"
 )
 
+func GetTrackById(c *gin.Context) {
+	trackId := c.Param("trackId")
+	var track Models.Track
+	err := Models.GetTrackById(trackId, &track)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+	} else {
+		c.JSON(http.StatusOK, track)
+	}
+}
+
 func GetTrackByGenre(c *gin.Context) {
 	query := c.Param("query")
 	query = strings.ReplaceAll(query, "+", " ")
@@ -59,4 +70,37 @@ func GetTrackHottest(c *gin.Context) {
 	} else {
 		c.JSON(http.StatusOK, tracks)
 	}
+}
+
+func GetTrackByUser(c *gin.Context) {
+	userId := c.Param("userId")
+	pageInfo := Models.NewPageInfo()
+	err := c.BindJSON(&pageInfo)
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	var tracks []Models.Track
+	err = Models.GetTrackByUser(&tracks, userId, pageInfo)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	c.JSON(http.StatusOK, tracks)
+}
+
+func IncreaseTrackField(c *gin.Context) {
+	var increaseTrackField Models.IncreaseTrackField
+	err := c.BindJSON(&increaseTrackField)
+	if err != nil {
+		c.AbortWithStatus(http.StatusBadRequest)
+		return
+	}
+	var track Models.Track
+	err = Models.IncreaseTrackPlayback(increaseTrackField, &track)
+	if err != nil {
+		c.AbortWithStatus(http.StatusNotFound)
+		return
+	}
+	c.JSON(http.StatusOK, track)
 }
--- api/Controllers/User.go
+++ api/Controllers/User.go
@@ -29,25 +29,9 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUserByID(c *gin.Context) {
-	id := c.Params.ByName("id")
+	userId := c.Params.ByName("id")
 	var user Models.User
-	err := Models.GetUserByID(&user, id)
-	if err != nil {
-		c.AbortWithStatus(http.StatusNotFound)
-	} else {
-		c.JSON(http.StatusOK, user)
-	}
-}
-
-func UpdateUser(c *gin.Context) {
-	var user Models.User
-	id := c.Params.ByName("id")
-	err := Models.GetUserByID(&user, id)
-	if err != nil {
-		c.JSON(http.StatusNotFound, user)
-	}
-	c.BindJSON(&user)
-	err = Models.UpdateUser(&user, id)
+	err := Models.GetUserByID(&user, userId)
 	if err != nil {
 		c.AbortWithStatus(http.StatusNotFound)
 	} else {
