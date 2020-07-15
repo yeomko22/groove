3575922d575073da965a53288be3345b1aa2d54d
ttumzzi <ssumini0829@gmail.com>
2020-07-15 10:34:13 +0900

    feat: 재생버튼 가운데로 고정

--- frontend/src/components/MusicPlayer/MusicPlayerSide.js
+++ frontend/src/components/MusicPlayer/MusicPlayerSide.js
@@ -3,11 +3,11 @@ import secToTimeFormat from '../../utils/secToTimeFormat';
 
 const MusicPlayerSide = ({ time, duration }) => {
   return (
-    <>
+    <div className="musiccontroller__rightside">
       <span className="musiccontroller__time">{`${secToTimeFormat(
         time,
       )} / ${secToTimeFormat(duration)}`}</span>
-    </>
+    </div>
   );
 };
 
