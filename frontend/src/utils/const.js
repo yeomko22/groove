6b73227de5b90461f9a088c8892f68abab115a2c
ttumzzi <ssumini0829@gmail.com>
2020-07-15 13:02:53 +0900

    style: 요구사항 수정(재생버튼 그림자 제거,  아티스트 추천멘트 변경)

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,7 +1,7 @@
 export const SERVER_IP = 'https://groov.space';
 export const categoryObject = {
   morning: [
-    ['tag', 'YELLA', 'Artist: YELLA'],
+    ['tag', 'YELLA', '오늘의 아티스트 추천: YELLA'],
     ['tag', 'music', '상쾌한 아침을 여는 음악'],
     ['hottest', '', `Charts: Groove's Weekly`],
     ['genre', 'lofi', 'Lofi'],
@@ -13,12 +13,12 @@ export const categoryObject = {
     ['tag', 'jazz', 'Jazz'],
   ],
   afternoon: [
-    ['tag', 'YELLA', 'Artist: YELLA'],
+    ['tag', 'YELLA', '오늘의 아티스트 추천: YELLA'],
     ['tag', '인디', '나른한 오후를 위한 음악'],
     ['newest', '', `what's new in Groove?`],
     ['tag', 'Newage', 'Newage'],
     ['tag', 'study', 'Study'],
-    ['tag', 'MonstaX', 'Artist: MontsaX'],
+    ['tag', 'MonstaX', 'Artist: MonstaX'],
     ['tag', 'ESBEE', 'Artist: ESBEE'],
     ['tag', 'Soul', 'Soul'],
     ['genre', 'pH-1', 'Artist: pH-1'],
@@ -28,7 +28,7 @@ export const categoryObject = {
   night: [
     ['hottest', '', 'Charts: Hottest'],
     ['tag', 'Mixtape', 'Night Party Music'],
-    ['genre', 'Paloalto', 'Artist: Paloalto'],
+    ['genre', 'Paloalto', '오늘의 아티스트 추천: Paloalto'],
     ['tag', 'lypla', 'Artist: Lypla'],
     ['genre', 'R&B', 'R&B'],
     ['tag', 'acoustic', 'Acoustic'],
@@ -37,7 +37,7 @@ export const categoryObject = {
     ['tag', 'YELLA', 'Artist: YELLA'],
   ],
   dawn: [
-    ['tag', 'YELLA', 'Artist: YELLA'],
+    ['tag', 'YELLA', '오늘의 아티스트 추천: YELLA'],
     ['tag', 'mood', '새벽 감성 음악'],
     ['newest', '', 'Charts: Newest'],
     ['genre', 'lofi', 'Lofi'],
