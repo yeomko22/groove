fdb12e13b0e993c26f0a31d9a1f1c044a149aff1
ttumzzi <ssumini0829@gmail.com>
2020-07-15 12:34:07 +0900

    feat: 슬라이더 지나간 부분 색상 변경

--- frontend/src/components/MusicPlayer/MusicPlayerSlider.js
+++ frontend/src/components/MusicPlayer/MusicPlayerSlider.js
@@ -1,12 +1,16 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState, useEffect, useCallback } from 'react';
 
 const MusicPlayerSlider = ({ time, duration, play, handleChangeTime }) => {
   const sliderRef = useRef();
+  const playedRef = useRef();
   const [sliderPosition, setSliderPosition] = useState(0);
+  const [sliderWidth, setSliderWidth] = useState(0);
 
   useEffect(() => {
-    if (!(duration === 0 || isNaN(duration)) && play)
+    if (!(duration === 0 || isNaN(duration)) && play) {
+      setSliderWidth(playedRef.current.offsetParent.clientWidth);
       setSliderPosition((time / duration) * 500);
+    }
   }, [time]);
 
   const hadleSliderChange = (e) => {
@@ -15,16 +19,23 @@ const MusicPlayerSlider = ({ time, duration, play, handleChangeTime }) => {
     setSliderPosition(position);
     handleChangeTime((position * duration) / 500);
   };
+
   return (
-    <input
-      className="musiccontroller__musicslider"
-      type="range"
-      ref={sliderRef}
-      min={0}
-      max={500}
-      value={sliderPosition}
-      onChange={hadleSliderChange}
-    />
+    <div className="musiccontroller__musicslider">
+      <div
+        className="musiccontroller__musicslider--progressbar"
+        ref={playedRef}
+        style={{ width: (sliderPosition / 500) * sliderWidth }}
+      ></div>
+      <input
+        type="range"
+        ref={sliderRef}
+        min={0}
+        max={500}
+        value={sliderPosition}
+        onChange={hadleSliderChange}
+      />
+    </div>
   );
 };
 
