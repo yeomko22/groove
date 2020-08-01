44a36b6fc6e26dbfabf26f53d0f7e3c6b02fc270
ttumzzi <44461473+ttumzzi@users.noreply.github.com>
2020-08-01 13:06:22 +0900

    fix: 디테일한 부분 수정 (#80)
    * feat: 메인 페이지 트랙 text hover 효과
    
    * fix: 디테일한 부분 수정중
    
    - user이름 클릭하면 해당 user페이지로 넘어감

--- frontend/src/components/Track/TrackPlayerInfo.js
+++ frontend/src/components/Track/TrackPlayerInfo.js
@@ -1,15 +1,27 @@
 import React from 'react';
 import TrackPlayerButton from './TrackPlayerButton';
 import WaveFormMain from '../WaveForm/WaveFormMain';
+import { Link } from 'react-router-dom';
 
 const TrackPlayerInfo = ({ track }) => {
-  const { TrackUserName, TrackTitle, TrackId, TrackDuration } = track;
+  const {
+    TrackUserName,
+    TrackTitle,
+    TrackId,
+    TrackDuration,
+    TrackUserId,
+  } = track;
   return (
     <div className="trackplayer__infocontainer">
       <div className="trackplayer__info">
         <TrackPlayerButton track={track} />
         <div>
-          <span className="trackplayer__info--text">{TrackUserName}</span>
+          <Link to={`/${TrackUserId}`}>
+            <span className="link trackplayer__info--text">
+              {TrackUserName}
+            </span>
+          </Link>
+
           <span className="trackplayer__info--text">{TrackTitle}</span>
         </div>
       </div>
